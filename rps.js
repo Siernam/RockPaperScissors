62b57c5a6322462a7bc04237b7126fd2635480ff
Siernam <siernam@gmail.com>
2024-01-09 14:06:35 -0500

    added user input to .js file

--- rps.js
+++ rps.js
@@ -1,9 +1,11 @@
 /Rock Paper Scissors Script/
 
-const selection = ["rock", "paper", "scissors"]
+const selection = ["rock", "paper", "scissors"];
+let playerSelection = prompt('Do you choose rock, paper, scissors');
 
 function getComputerChoice() {
     console.log(selection[(Math.floor(Math.random() * selection.length))]);
     }
-getComputerChoice()
+getComputerChoice();
 
+let computerSelection = getComputerChoice();
