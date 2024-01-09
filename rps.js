cd7414b005b2cdd103c5aad15c462ceba65ab285
Siernam <siernam@gmail.com>
2024-01-09 17:10:23 -0500

    set a single round in .js file tested several times

--- rps.js
+++ rps.js
@@ -2,10 +2,48 @@
 
 const selection = ["rock", "paper", "scissors"];
 let playerSelection = prompt('Do you choose rock, paper, scissors');
+console.log(playerSelection);
+
 
 function getComputerChoice() {
-    console.log(selection[(Math.floor(Math.random() * selection.length))]);
+    return(selection[(Math.floor(Math.random() * selection.length))]);
     }
 getComputerChoice();
 
-let computerSelection = getComputerChoice();
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+function compare(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        return "The result is a tie!";
+    }
+    if (computerSelection === "rock") {
+        if (playerSelection === "scissors") {
+            return "rock wins";
+        } else { 
+            if (playerSelection === "paper") {
+                return "paper wins";
+            }
+        }
+    }
+    if (computerSelection === "paper") {
+        if (playerSelection === "rock") {
+            return "paper wins";
+        } else {
+            if (playerSelection === "scissors") {
+                return "scissors wins";
+            }
+        }
+    }
+    if (computerSelection === "scissors") {
+        if (playerSelection === "rock") {
+            return "rock wins";
+        } else {
+            if (playerSelection === "paper") {
+                return "scissors wins";
+                }
+            }
+        }
+    };
+
+console.log(compare(computerSelection,playerSelection));
